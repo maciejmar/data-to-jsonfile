c76b26849113eba27f18539fc37226fa62bf76f7
Maciej Marosz <48849533+maciejmar@users.noreply.github.com>
2022-07-03 06:20:43 +0200

    changes in db model for Nicpon purposes

--- backend/models/dialog.js
+++ backend/models/dialog.js
@@ -1,26 +1,32 @@
 const mongoose  = require('mongoose');
 
 const dialogSchema = new mongoose.Schema({
-  sectionName: {
+  dialogName: {
     type: String,
     required: true
   },
-  textName: {
-    type: String,
-    required: true
-  },
-  pl:{
-    type: String,
-    required: true
-  },
-  en:{
-    type: String,
-    required: true
-  },
-  de:{
-    type: String,
-    required: true
-  }
+  part:[{
+      textName: {
+        type: String,
+        required: true
+      },
+      fileName: {
+        type: String,
+        required: true
+      },
+      pl:{
+        type: String,
+        required: true
+      },
+      en:{
+        type: String,
+        required: true
+      },
+      de:{
+        type: String,
+        required: true
+     }
+  }]
 
 
 });
