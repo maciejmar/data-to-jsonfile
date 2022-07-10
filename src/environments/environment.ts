6bedf3ba0ae43c47fdc97b74dfafe6de439e60d8
Maciej Marosz <48849533+maciejmar@users.noreply.github.com>
2022-07-10 16:34:51 +0200

    enviroment production true changed from false

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   URL_DIALOGS: 'http://localhost:3000/dialogs',
   URL_SECTIONS: 'http://localhost:3000/sections'
 };
